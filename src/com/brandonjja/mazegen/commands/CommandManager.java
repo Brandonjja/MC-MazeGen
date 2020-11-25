d5408020a7f675c003816be676c4c996bcce741a
Brandon <brandonjja@hotmail.com>
2020-11-25 01:14:01 -0400

    Added /mazewand command

--- src/com/brandonjja/mazegen/commands/CommandManager.java
+++ src/com/brandonjja/mazegen/commands/CommandManager.java
@@ -10,6 +10,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.brandonjja.mazegen.commands.handler.GenerateCommand;
+import com.brandonjja.mazegen.commands.handler.MazeWandCommand;
 
 public class CommandManager implements CommandExecutor {
 	private static Map<String, MazeCommand> commandList = new HashMap<>();
@@ -24,6 +25,7 @@ public class CommandManager implements CommandExecutor {
 	
 	public static void registerCommands() {
 		commandList.put("generate", new GenerateCommand());
+		commandList.put("mazewand", new MazeWandCommand());
 		
 		for (String cmdLabel : commandList.keySet()) {
 			register(cmdLabel, new CommandManager());

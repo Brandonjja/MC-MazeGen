62cfa2557ff54fd8cc5105b14c61c02f703d4ff3
Brandon <brandonjja@hotmail.com>
2020-11-25 01:17:53 -0400

    Added /generate command

--- src/com/brandonjja/mazegen/commands/handler/GenerateCommand.java
+++ src/com/brandonjja/mazegen/commands/handler/GenerateCommand.java
@@ -2,13 +2,28 @@ package com.brandonjja.mazegen.commands.handler;
 
 import org.bukkit.entity.Player;
 
+import com.brandonjja.mazegen.api.MazeAPI;
 import com.brandonjja.mazegen.commands.MazeCommand;
 
-public class GenerateCommand extends MazeCommand {
+import net.md_5.bungee.api.ChatColor;
 
+public class GenerateCommand extends MazeCommand {
+	
+	final String invalidBlock = ChatColor.RED + "Please enter a valid block";
+	
 	@Override
 	public boolean execute(Player player, String[] args) {
-		return false;
+		if (args.length != 1) return false;
+		try {
+			MazeAPI.generateMaze(args[0]);
+		} catch (NullPointerException ex) {
+			ex.printStackTrace();
+			return false;
+		} catch (IllegalArgumentException ex) {
+			player.sendMessage(invalidBlock);
+			return true;
+		}
+		return true;
 	}
 
 }
